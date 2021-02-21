fef922835f7acdc16c7d3acb810338cd384df433
Xeletron <xeletron1@gmail.com>
2021-02-21 18:42:17 +0100

    Contest Final

--- hooks/InteractionTweakData.lua
+++ hooks/InteractionTweakData.lua
@@ -1,6 +1,5 @@
 Hooks:PostHook(InteractionTweakData, "init", "hwu_manhole_interact", function(self, tweak_data)
 	
-	self.sewer_manhole.special_equipment = "crowbar"
 	self.sewer_manhole.sound_start = "bar_crowbar_open_metal"
 	self.sewer_manhole.sound_interupt = "bar_crowbar_open_metal_cancel"
 	self.sewer_manhole.sound_interupt = "bar_crowbar_open_metal_finished"
