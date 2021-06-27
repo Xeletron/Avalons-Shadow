e1e48bbfed1f579a55ac1290d806a78f9529215d
Xeletron <xeletron1@gmail.com>
2021-06-27 21:02:38 +0200

    Updated for editor DB update

--- hooks/InteractionTweakData.lua
+++ hooks/InteractionTweakData.lua
@@ -1,9 +1,4 @@
 Hooks:PostHook(InteractionTweakData, "init", "hwu_manhole_interact", function(self, tweak_data)
-	
-	self.sewer_manhole.sound_start = "bar_crowbar_open_metal"
-	self.sewer_manhole.sound_interupt = "bar_crowbar_open_metal_cancel"
-	self.sewer_manhole.sound_interupt = "bar_crowbar_open_metal_finished"
-
 	self.hold_charge_gun.text_id = "hud_int_hold_open_hatch"
 	self.hold_charge_gun.action_text_id = "hud_action_opening_hatch"
 end)
