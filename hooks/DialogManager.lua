4fb323de713d99cf992c28e888e0e03d1fffca17
Xeletron <xeletron1@gmail.com>
2021-07-20 20:21:22 +0200

    New underground voicelines

--- hooks/DialogManager.lua
+++ hooks/DialogManager.lua
@@ -1,6 +1,6 @@
 Hooks:PostHook(DialogManager, "_play_dialog", "hwu_dialog_start", function()
     if Global.music_manager.current_track and Global.music_manager.current_event then
-        SoundDevice:set_rtpc("option_music_volume", Global.music_manager.volume * 0.42 * 100)
+        SoundDevice:set_rtpc("option_music_volume", Global.music_manager.volume * 0.37 * 100)
     end
 end)
 
